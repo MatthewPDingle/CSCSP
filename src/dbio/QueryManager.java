000aea1362ca0aeed6ab04931159edd678d4b574
MatthewPDingle <matthew.p.dingle@gmail.com>
2015-07-23 20:11:13 -0500

    Another .39 Bug fix

--- src/dbio/QueryManager.java
+++ src/dbio/QueryManager.java
@@ -1013,9 +1013,8 @@ public class QueryManager {
 	public static void deleteStocksFromBar() {
 		try {
 			Connection c = ConnectionSingleton.getInstance().getConnection();
-			String q = "DELETE FROM " + Constants.BAR_TABLE + " b " +
-					   "INNER JOIN indexlist i ON b.symbol = i.symbol " +
-					   "WHERE i.index = 'NYSE' OR i.index = 'Nasdaq' OR i.index = 'DJIA' OR i.index = 'SP500' OR i.index = 'ETF' OR i.index = 'Stock Index'";
+			String q = "DELETE FROM bar " +
+						"WHERE symbol IN (SELECT symbol FROM indexlist WHERE index = 'NYSE' OR index = 'Nasdaq' OR index = 'DJIA' OR index = 'SP500' OR index = 'ETF' OR index = 'Stock Index') ";
 			Statement s = c.createStatement();
 			s.executeUpdate(q);
 			s.close();
@@ -1462,13 +1461,10 @@ public class QueryManager {
 	public static void deleteHolidaysForStocksFromBar() {
 		try {
 			Connection c = ConnectionSingleton.getInstance().getConnection();
-			String q = 	"DELETE FROM bar b " +
-						"INNER JOIN indexlist i ON b.symbol = i.symbol " +
-						"WHERE i.index = 'NYSE' OR i.index = 'Nasdaq' OR i.index = 'DJIA' OR i.index = 'SP500' OR i.index = 'ETF' OR i.index = 'Stock Index' " +
-						"AND b.date IN (SELECT * FROM " +
-						  	"(SELECT * FROM generate_series('2012-01-01'::date, now()::date, '1 day') AS alldates) t " +
-						  	"WHERE alldates NOT IN (SELECT * FROM tradingdays))";
-
+			String q = 	"DELETE FROM bar " +
+						"WHERE symbol IN (SELECT symbol FROM indexlist WHERE index = 'NYSE' OR index = 'Nasdaq' OR index = 'DJIA' OR index = 'SP500' OR index = 'ETF' OR index = 'Stock Index') " +
+						"AND start IN (SELECT * FROM (SELECT * FROM generate_series('2012-01-01'::date, now()::date, '1 day') AS alldates) t WHERE alldates NOT IN (SELECT * FROM tradingdays))";
+		
 			PreparedStatement ps = c.prepareStatement(q);
 			ps.executeUpdate();
 			ps.close();
