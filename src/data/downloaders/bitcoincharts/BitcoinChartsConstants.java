2243592841d18bfcac49223b917833c0bc11f3f5
Matt Dingle <zottower@Desktop.attlocal.net>
2015-07-27 16:55:48 -0500

    Added BitcoinCharts archive downloader

---
+++ src/data/downloaders/bitcoincharts/BitcoinChartsConstants.java
@@ -0,0 +1,14 @@
+package data.downloaders.bitcoincharts;
+
+public class BitcoinChartsConstants {
+
+	public static String URL = "http://api.bitcoincharts.com/v1/csv/";
+	
+	public static String FILE_TICK_HISTORY_BITFINEX_BTC_USD = "bitfinexUSD.csv.gz";
+	public static String FILE_TICK_HISTORY_BITSTAMP_BTC_USD = "bitstampUSD.csv.gz";
+	public static String FILE_TICK_HISTORY_BTCE_BTC_USD = "btceUSD.csv.gz";
+	public static String FILE_TICK_HISTORY_BTCN_BTC_CNY = "btcnCNY.csv.gz";
+	public static String FILE_TICK_HISTORY_KRAKEN_BTC_USD = "krakenUSD.csv.gz";
+	public static String FILE_TICK_HISTORY_KRAKEN_BTC_EUR = "krakenEUR.csv.gz";
+	public static String FILE_TICK_HISTORY_OKCOIN_BTC_CNY = "okcoinCNY.csv.gz";
+}
---
+++ src/data/downloaders/bitcoincharts/BitcoinChartsDownloader.java
@@ -0,0 +1,32 @@
+package data.downloaders.bitcoincharts;
+
+import java.io.File;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+
+import constants.Constants.BAR_SIZE;
+import data.Converter;
+
+public class BitcoinChartsDownloader {
+
+	public static void main(String[] args) {
+
+		downloadArchive(BitcoinChartsConstants.FILE_TICK_HISTORY_KRAKEN_BTC_EUR);
+		Converter.processTickDataIntoBars("krakenBTCEUR", BAR_SIZE.BAR_15M);
+	}
+
+	public static void downloadArchive(String fileName) {
+		try {
+			FileUtils.copyURLToFile(new URL(BitcoinChartsConstants.URL + fileName), new File("data/" + fileName));
+			
+			LogManager.getLogger("data.downloader").info("Downloaded file " + fileName + " from BitcoinCharts");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			LogManager.getLogger("data.downloader").error("Error downloading file " + fileName + " from BitcoinCharts");
+			LogManager.getLogger("data.downloader").error(e.getStackTrace().toString());
+		}
+	}	
+}
