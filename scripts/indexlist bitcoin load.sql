0d7772bb394781745a4fd09b8a53325427d6b2cb
Matt <matthew.p.dingle@gmail.com>
2015-07-28 21:11:52 -0500

    Half way through making the GUI a little bigger to support additional filters needed for selecting bar size & symbols.

---
+++ scripts/indexlist bitcoin load.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO indexlist(symbol, index)
+VALUES 
+('bitfinexBTCUSD', 'Bitcoin'),
+('bitstampBTCUSD', 'Bitcoin'),
+('btceBTCUSD', 'Bitcoin'),
+('btcnBTCCNY', 'Bitcoin'),
+('krakenBTCUSD', 'Bitcoin'),
+('krakenBTCEUR', 'Bitcoin'),
+('okcoinBTCCNY', 'Bitcoin'),
+('okcoinBTCUSD', 'Bitcoin'),
+('okcoinLTCCNY', 'Bitcoin'),
+('okcoinLTCUSD', 'Bitcoin')
+
+
--- src/data/TickConstants.java
+++ src/data/TickConstants.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.ArrayList;
+
 public class TickConstants {
 
 	public static String TICK_NAME_BITFINEX_BTC_USD = "bitfinexBTCUSD";
@@ -16,4 +18,19 @@ public class TickConstants {
 	public static String TICK_NAME_OKCOIN_BTC_USD = "okcoinBTCUSD";
 	public static String TICK_NAME_OKCOIN_LTC_CNY = "okcoinLTCCNY";
 	public static String TICK_NAME_OKCOIN_LTC_USD = "okcoinLTCUSD";
+	
+	public static ArrayList<String> BITCOIN_TICK_NAMES = new ArrayList<String>();
+	
+	static {
+		BITCOIN_TICK_NAMES.add(TICK_NAME_BITFINEX_BTC_USD);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_BITSTAMP_BTC_USD);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_BTCE_BTC_USD);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_BTCN_BTC_CNY);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_KRAKEN_BTC_USD);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_KRAKEN_BTC_EUR);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_OKCOIN_BTC_CNY);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_OKCOIN_BTC_USD);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_OKCOIN_LTC_CNY);
+		BITCOIN_TICK_NAMES.add(TICK_NAME_OKCOIN_LTC_USD);
+	}
 }
