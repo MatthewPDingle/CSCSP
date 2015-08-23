26232a72f0e4d96a20079946f17c1ecffae57ca7
Matt <matthew.p.dingle@gmail.com>
2015-08-23 09:19:03 -0500

    Added models table script

---
+++ scripts/models.sql
@@ -0,0 +1,57 @@
+﻿-- Table: models
+
+-- DROP TABLE models;
+
+CREATE TABLE models
+(
+  id serial NOT NULL,
+  modelfile text,
+  algo text,
+  params text,
+  symbol text,
+  duration text,
+  metrics text[],
+  trainstart timestamp without time zone,
+  trainend timestamp without time zone,
+  teststart timestamp without time zone,
+  testend timestamp without time zone,
+  sellmetric text,
+  sellmetricvalue real,
+  stopmetric text,
+  stopmetricvalue real,
+  numbars integer,
+  traindatasetsize integer,
+  traintruenegatives integer,
+  trainfalsenegatives integer,
+  trainfalsepositives integer,
+  traintruepositives integer,
+  traintruepositiverate real,
+  trainfalsepositiverate real,
+  traincorrectrate real,
+  trainkappa real,
+  trainmeanabsoluteerror real,
+  trainrootmeansquarederror real,
+  trainrelativeabsoluteerror real,
+  trainrootrelativesquarederror real,
+  trainrocarea real,
+  testdatasetsize integer,
+  testtruenegatives integer,
+  testfalsenegatives integer,
+  testfalsepositives integer,
+  testtruepositives integer,
+  testtruepositiverate real,
+  testfalsepositiverate real,
+  testcorrectrate real,
+  testkappa real,
+  testmeanabsoluteerror real,
+  testrootmeansquarederror real,
+  testrelativeabsoluteerror real,
+  testrootrelativesquarederror real,
+  testrocarea real,
+  CONSTRAINT models_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE models
+  OWNER TO postgres;
